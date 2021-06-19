c6cc3a911e69fff6273a2fd851285f5f5a886e2f
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-06-19 12:24:39 +0700

    Add layout

--- CamIPStore/Startup.cs
+++ CamIPStore/Startup.cs
@@ -49,6 +49,9 @@ namespace CamIPStore
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "MyArea",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
