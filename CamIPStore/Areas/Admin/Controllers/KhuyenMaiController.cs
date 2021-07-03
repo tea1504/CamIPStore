104fc54d565fa04ad9e154969832bd1d8dabaa57
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-07-03 10:22:06 +0700

    Hòa Create KhuyenMaiConTroler in Admin area to magane field KhuyenMai

---
+++ CamIPStore/Areas/Admin/Controllers/KhuyenMaiController.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CamIPStore.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class KhuyenMaiController : Controller
+    {
+        private readonly IPShopDBContext _context;
+        public KhuyenMaiController(IPShopDBContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
